ee849a0012563d301dac1b045bb64d952142ff96
Bismark <bismarkb609@gmail.com>
2024-03-31 11:20:04 +0000

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-A Fully functional Dashboard application
+<h2>Dashboard application</h2>
+<br >
+<img src="./home.png" width="100%" alt="dasher home page" />
+<hr >
+
+<h4>Description</h4>
+Dashboard app built with NextJS with server actions<br >
+Feel free to play with it as you wish
+<br >
+
+<h4>Installation Guide</h4>
+I'll assume you already have <strong>Node</strong> installed.
+
+Start by cloning the repo:
+
+```
+https://github.com/bismarkboateng/nextAdmin.git
+
+cd nextAdmin
+
+yarn
+
+yarn dev
+
+```
+
+select the link running on your development server to preview<br>
+the application
--- authconfig.js
+++ authconfig.js
@@ -4,16 +4,16 @@ export const authConfig = {
     signIn: "/login",
   },
   callbacks: {
-    authorized({auth, request}) {
-        const isLoggedIn = auth?.user
-        const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard")
-        if (isOnDashboard) {
-            if (isLoggedIn) return true
-            return false
-        } else if (isLoggedIn) {
-            return Response.redirect(new URL("/dashboard", request.nextUrl))
-        }
-        return true 
-    }
+    // authorized({auth, request}) {
+    //     const isLoggedIn = auth?.user
+    //     const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard")
+    //     if (isOnDashboard) {
+    //         if (isLoggedIn) return true
+    //         return false
+    //     } else if (isLoggedIn) {
+    //         return Response.redirect(new URL("/dashboard", request.nextUrl))
+    //     }
+    //     return true 
+    // }
   }
 };
